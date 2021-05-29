ebe3bdfba465104bc92aba55e204902376d69cd1
GeonNamGung <rndrjs123@naver.com>
2021-05-29 17:35:28 +0900

    add voca hitRate to VocaListActivity

--- app/src/main/java/kr/ac/konkuk/wordnote/VocaRecylcerViewAdapter.kt
+++ app/src/main/java/kr/ac/konkuk/wordnote/VocaRecylcerViewAdapter.kt
@@ -1,9 +1,11 @@
 package kr.ac.konkuk.wordnote
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class VocaRecylcerViewAdapter(
@@ -11,23 +13,32 @@ class VocaRecylcerViewAdapter(
 ) : RecyclerView.Adapter<VocaRecylcerViewAdapter.ViewHolder>() {
 
 
-    var onItemSelected: ((voca: Voca)-> Unit)? = null
+    lateinit var context: Context
+    var onItemSelected: ((voca: Voca) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_voca, parent, false)
+        context= parent.context
+
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
         holder.itemView.setOnClickListener {
-            onItemSelected?.run{
+            onItemSelected?.run {
                 this(item)
             }
         }
-        holder.wordView.text= item.word
-        holder.meaingView.text= item.meaning
+        holder.wordView.text = item.word
+        holder.meaingView.text = item.meaning
+        holder.hitRateView.text = "정답률: ${"%.2f".format(item.getHitRate()*100)}%"
+        if(item.getHitRate()>= 0.7){
+            holder.hitRateView.setTextColor(ContextCompat.getColor(context, R.color.right))
+        }else{
+            holder.hitRateView.setTextColor(ContextCompat.getColor(context, R.color.wrong))
+        }
     }
 
 
@@ -36,5 +47,6 @@ class VocaRecylcerViewAdapter(
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val wordView: TextView = view.findViewById(R.id.word)
         val meaingView: TextView = view.findViewById(R.id.meaning)
+        val hitRateView: TextView = view.findViewById(R.id.hitrate)
     }
 }

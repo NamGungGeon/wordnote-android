9580397984382b66d9109c5fd602d48613f69cc0
namgung-geon <rndrjs123@naver.com>
2021-06-04 21:45:53 +0900

    fix typo

--- app/src/main/java/kr/ac/konkuk/wordnote/InstantVocaFragment.kt
+++ app/src/main/java/kr/ac/konkuk/wordnote/InstantVocaFragment.kt
@@ -8,13 +8,13 @@ import android.widget.ImageButton
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 
-class InstanceVocaFragment : Fragment() {
+class InstantVocaFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instance_voca, container, false)
+        return inflater.inflate(R.layout.fragment_instant_voca, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
